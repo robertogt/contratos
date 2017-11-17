ba5a77cc6548674db56bbbc188d93d133e7a64f4
jrroquel <jrroquel@contraloria.gob.gt>
2017-11-17 11:38:19 -0600

    Funcionalidad completa. Falta validar al ingresar el monto, muestre mensaje de falta de presupuesto

--- src/main/ngapp/src/app/addendum/addendum.component.ts
+++ src/main/ngapp/src/app/addendum/addendum.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { RueService } from '../services/rue.service';
+import { Laboral } from '../model/laboral';
+import { NgForm } from '@angular/forms';
+import {Message} from 'primeng/primeng';
 
 @Component({
   selector: 'app-addendum',
@@ -7,9 +11,109 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddendumComponent implements OnInit {
 
-  constructor() { }
+	actividades: any[];
+	contratista: any;
+	data: any={};
+	fechaDel: any;
+  	fechaAl: any;
+  	idPerfil: any = null;
+  	msgs: Message[] = [];
+  	perfiles: any[];
+  	personas: any[];
+  	renglon: any;
+	renglones: any[];
+	tipoServicios: any;
+	tiposServicios: any[];
+	ubicacion: any={};
+	ubicaciones: any[];
+  	
+  	constructor(private rueService:RueService) { }
 
-  ngOnInit() {
-  }
+  	ngOnInit() {
+  		this.tiposServicios = [{value:'T',label:'Tecnicos'},{value:'P',label:'Profesionales'}]
+		this.rueService.getRenglones().subscribe(renglones => this.renglones = renglones);
+  	}
+
+  	cargaDatos(data){
+
+	    this.renglon = data.renglon;
+	    this.tipoServicios = data.tipoServicios;
+	    this.idPerfil = data.iPerfil;
+	    this.ubicacion.idUbicacionFuncional = data.ubicacionFuncional;
+	    (<HTMLInputElement>document.getElementById("ubica")).value = data.nombreUbicacion;
+	    this.getPerfiles();
+  	}
+
+  	cargaDatosDelFuncionario(data){
+    
+    if (data.code!=200)
+      this.muestraMensaje('error',data.message);    
+    else{
+      	this.data = data.data;
+      	this.cargaDatos(this.data);
+    	}
+
+	}
+
+	getActividades(perfil){
+
+      this.rueService
+      .getActividadesPorPerfil(perfil,this.data.idRue)
+      .subscribe(  actividades => {this.actividades = actividades;},
+                error => { var errorMessage = <any>error; console.log(errorMessage);});
+  	}
+
+	getPerfiles(){
+    
+  		this.rueService
+  		.getPerfilesPorUbicacion(this.ubicacion.idUbicacionFuncional)
+  		.subscribe(	perfiles => { this.perfiles = perfiles;
+                                this.actividades = [];
+                                
+                                if(this.data.actividades != undefined)
+                                {
+                                  this.actividades = this.data.actividades;
+                                }
+                              },
+            		error => { var errorMessage = <any>error; console.log(errorMessage);});
+  	}
+
+  	inicializaLaboral(data){
+    	this.cargaDatosDelFuncionario(data);    
+  	}
+
+  	muestraMensaje(tipoMensaje, message){
+      this.msgs.push({severity:tipoMensaje, summary:'', detail:message});
+  	}
+
+  	search(event) {
+        this.rueService
+        .getPersonas(event.query)
+        .subscribe(	result => { this.personas = result;}, 
+        			error => {var errorMessage = <any>error;console.log(errorMessage);});
+	}
+
+	searchUbicacion(event) {
+        this.rueService
+        .getUbicacionesFuncionales(event.query)
+        .subscribe(	ubicaciones => {this.ubicaciones = ubicaciones;},
+            		error => { var errorMessage = <any>error; console.log(errorMessage);});
+  	}
+
+	seleccionaContratista(){
+      this.rueService
+      .getFuncionario(this.contratista.dpi)
+      .subscribe(  data => {console.log('data',data); this.inicializaLaboral(data);},
+                error => { var errorMessage = <any>error; console.log(errorMessage);});
+  	}
+
+  	selectPerfil(perfil){
+      this.idPerfil = perfil.idPerfil;
+  		this.getActividades(this.idPerfil);
+  	}
+  	
+  	seleccionaUbicacion(){
+  		this.getPerfiles();
+  	}
 
 }
